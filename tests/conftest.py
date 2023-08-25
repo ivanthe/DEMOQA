b34b6df25af6e18ffcc754c93e219916c652a3ed
ivanthe <x6027858x@gmail.com>
2023-08-25 12:59:33 +0300

    добавил тест по апдейту персональных данных

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,14 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome('\driver\chromedriver')
-    #driver = webdriver.Chrome(ChromeDriverManager().install())
+    #driver = webdriver.Chrome('\driver\chrome')
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
     driver.maximize_window()
     yield driver
     driver.quit()
